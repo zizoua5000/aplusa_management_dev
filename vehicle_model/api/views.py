fbc1f972342d5f53fac698ac04f40e2a49f81126
asadmammadov <asadmammadov@gmail.com>
2020-04-29 13:20:19 +0400

    order by added to vecile model list

--- vehicle_model/api/views.py
+++ vehicle_model/api/views.py
@@ -10,7 +10,7 @@ from rest_framework.permissions import (
 
 class VehicleModelListCreateAPIView(ListCreateAPIView):
     permission_classes = (CustomDjangoModelPermissions, )
-    queryset=VehicleModel.objects.all()
+    queryset=VehicleModel.objects.all().order_by('id')
     serializer_class=VehicleModelSerializer
     
     # permission_classes=[IsAuthenticated]
