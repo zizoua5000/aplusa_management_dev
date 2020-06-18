128a6a55e5521efb3a05bcd9f586532df3739e94
Nizam <gahramanovnizam@gmail.com>
2020-06-18 11:40:48 +0400

    vehiclefilter

--- vehicle/api/filters.py
+++ vehicle/api/filters.py
@@ -3,12 +3,18 @@ from vehicle.api.models import Vehicle
 
 class NumberInFilter(filter.BaseInFilter, filter.NumberFilter,):
     pass
+class CharInFilter(filter.BaseInFilter, filter.CharFilter):
+    pass
 
 class VehicleFilter(filter.FilterSet):
-    plate = filter.CharFilter(field_name='plate',lookup_expr='iexact')
-    serie_number = filter.CharFilter(field_name='serie_number', lookup_expr='iexact') 
+    id = NumberInFilter(field_name='id',lookup_expr='in')
+    plate = CharInFilter(field_name='plate',lookup_expr='in')
+    serie_number = CharInFilter(field_name='serie_number', lookup_expr='in') 
+    comment = filter.CharFilter(lookup_expr='icontains')
+    vehicle_model = NumberInFilter(field_name='vehicle_model', lookup_expr='in')
+    vehicle_type = NumberInFilter(field_name='vehicle_type', lookup_expr='in')
 
     class Meta:
         model = Vehicle
-        fields = ['plate','serie_number','id']
+        fields = ['plate','serie_number','id','comment','vehicle_model','vehicle_type']
 
