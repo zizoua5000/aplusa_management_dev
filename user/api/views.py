0e30b3e41ef93d5da619449419c8c80d7612d396
Nicat Talibov <nijat.t@aplusa-security.com>
2020-06-30 11:40:35 +0400

    User using in person

--- user/api/views.py
+++ user/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework import status
+from rest_framework.response import Response
 from user.api.serializers import UserRegisterSerializer,UserUpdateDeleteSerializer
 from django.contrib.auth.models import User
+from person.api.models import Person
 from aplusa_management.permissions import CustomDjangoModelPermissions
 from rest_framework.permissions import (
     IsAuthenticated,
@@ -18,3 +21,10 @@ class UserUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
     queryset=User.objects.all()
     serializer_class=UserUpdateDeleteSerializer
     # permission_classes=[IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if Person.objects.filter(user=instance.id).first():
+            return Response("This is using in another table", status=status.HTTP_400_BAD_REQUEST)
+        self.perform_destroy(instance)
+        return Response("Deleted", status=status.HTTP_200_OK)
